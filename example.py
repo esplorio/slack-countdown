16647de24d2e0a664c95291337094f990bcfb96c
Denvar <denvar.94@gmail.com>
2015-07-18 17:14:32 +0100

    checks

---
+++ example.py
@@ -0,0 +1,14 @@
+def incr(num):
+    return num+1
+
+def addFive(num):
+    return num+5
+
+def main():
+    # my code here
+    print incr(1)
+    print addFive(1)
+    
+    
+if __name__ == "__main__":
+    main()
